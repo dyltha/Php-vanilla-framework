a72d42d97b8bac77ae34a4a7bbe36d9e681e4f7a
Matthis HOULES <48548675+dyltha@users.noreply.github.com>
2020-09-04 16:26:25 +0200

    Adding V1

---
+++ App/Controller/templateC.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 
+ *  @name : templateC.php
+ *  
+ *  @brief :  Example controller pages
+ * 
+ * 
+ */
+
+
+
+// required models
+// require_once(__DIR__.'/../../Core/PopUp.php');
+
+// session_start ?
+// session_start();
+
+ class TemplateC {
+
+
+    /**
+     *  @name : exampleMethod
+     *  @param : void
+     * 
+     *  @return : void
+     *
+     * 
+     */
+    public function exampleMethod() {
+        var_dump('example');
+
+    }// public function createCar()
+
+}
---
+++ App/Model/template.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *  @title : template.php
+ * 
+ *  @brief : Example model
+ * 
+ */
+class Template extends Model{
+
+    // class vars
+    // protected $var;
+
+
+    /**
+     *  @name : __construct
+     * 
+     *  @brief : constructor
+     * 
+     */
+    function __construct() {
+    
+    }
+
+}
+
+
+
+?>
