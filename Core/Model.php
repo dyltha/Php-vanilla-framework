79b801ce46e6523e4a26fa4920659dfedfecbad8
Matthis HOULES <48548675+dyltha@users.noreply.github.com>
2020-09-14 14:30:50 +0200

    Fix Model class

--- Core/Model.php
+++ Core/Model.php
@@ -11,19 +11,27 @@ require_once(__DIR__.'/Config.php');
 
 abstract class Model {
 
-    static $database = null;
+    static protected $database = null;
 
 
+    /**
+     * 
+     * @name : DBConnect
+     * 
+     * @param void
+     * @return PDO
+     * 
+     * @brief : connection to database
+     * 
+     */
     protected static function DBConnect() {
-        if ($database === null) {
+        $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
 
-            $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME . ';charset=utf8';
+        $database = new PDO($dsn, Config::DB_USER, Config::DB_PWD);
 
-            $this->database = new PDO($dsn, Config::DB_USER, Config::DB_PWD);
+        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        }
+        return $database;
     }
 
 
